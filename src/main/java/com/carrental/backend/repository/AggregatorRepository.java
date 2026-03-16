49b4b6f4fc3c35019ef2b50d715e2c7aac496c80
Cayenne <cayenne@Cayennes-MacBook-Air.local>
2026-03-16 17:36:56 +0400

    Remove aggregator domain to fix compilation

--- src/main/java/com/carrental/backend/repository/AggregatorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.carrental.backend.repository;
-
-import com.carrental.backend.model.Aggregator;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AggregatorRepository extends JpaRepository<Aggregator, String> {
-}
