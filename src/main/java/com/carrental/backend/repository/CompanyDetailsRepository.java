fffb3a301463361ed63e1d87ce70dce412990f58
Cayenne <cayenne@Cayennes-MacBook-Air.local>
2026-03-27 14:20:47 +0400

    Restore original pom.xml and remove PDF generation code

--- src/main/java/com/carrental/backend/repository/CompanyDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.carrental.backend.repository;
-
-import com.carrental.backend.model.CompanyDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyDetailsRepository extends JpaRepository<CompanyDetails, String> {
-}
